1448df30cceed7d8c0d358f0ec252d8ebd7acd19
Киселев Михаил В. <mvkiselev@group.legion.ru>
2026-05-15 20:51:54 +0300

    File structure improvements

---
+++ app/schemas/security.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+from pydantic import BaseModel
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TelegramLoginRequest(BaseModel):
+    telegram_id: int
+    telegram: str
+    bot_secret: str
